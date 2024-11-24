1c6be093f4d2eb002ededccace0b99fa4e4559ea
adamrifs <rifsadam81@gmail.com>
2024-11-24 17:07:42 +0530

    updates

--- src/components/datafile.jsx
+++ src/components/datafile.jsx
@@ -7,73 +7,82 @@ import LuxeRooms from './assets/LuxeRooms.png'
 import adventure from './assets/adventure.png'
 import UrbanAbodes from './assets/urbanabodes.png'
 import Humanica from './assets/Humanica.png'
+import Clove from './assets/clove.png'
 export const projects = [
     {
-        id:'1',
+        id: '1',
         image: fornax,
-        link:'https://fornaxgamestore.vercel.app/',
-        name:'Fornax gaming store',
-        description:'Fornax Gaming Store is an advanced gaming store developed using <b>React.js</b>.It features a secure user registration and login system, comprehensive product listings with filters and search functionality,and detailed product information. Users can manage their profiles, maintain a wishlist, and utilize a shopping cart for a seamless shopping experience.Additionally, an admin dashboard facilitates efficient store management.This platform offers a complete and user-friendly solution for gaming enthusiasts.'
+        link: 'https://fornaxgamestore.vercel.app/',
+        name: 'Fornax gaming store',
+        description: 'Fornax Gaming Store is an advanced gaming store developed using <b>React.js</b>.It features a secure user registration and login system, comprehensive product listings with filters and search functionality,and detailed product information. Users can manage their profiles, maintain a wishlist, and utilize a shopping cart for a seamless shopping experience.Additionally, an admin dashboard facilitates efficient store management.This platform offers a complete and user-friendly solution for gaming enthusiasts.'
     },
     {
-        id:'2',
+        id: '2',
+        image: Clove,
+        link: 'https://clove-international.vercel.app/',
+        name: 'Clove International',
+        description: 'Clove International is a Dubai-based investment platform designed for seamless money investment. <b>Developed as a freelance project</b>, it features a user-friendly dashboard for managing investment plans, a secure login and signup system, and advanced settings and additional functionalities to enhance the user experience.'
+    },
+    {
+        id: '3',
         image: university,
-        link:'https://adamrifs.github.io/university',
-        name:'University web page',
-        description:'Developed using <b>Python Django</b>, this University web page offers a secure login system and a range of features.Users can select and enroll in different courses, view detailed course information, and manage their profiles effortlessly.The platform ensures a user-friendly and secure environment,streamlining academic activities for students and faculty.'
+        link: 'https://adamrifs.github.io/university',
+        name: 'University web page',
+        description: 'Developed using <b>Python Django</b>, this University web page offers a secure login system and a range of features.Users can select and enroll in different courses, view detailed course information, and manage their profiles effortlessly.The platform ensures a user-friendly and secure environment,streamlining academic activities for students and faculty.'
     },
     {
-        id:'3',
-        image:adventure,
-        link:'https://adventuretemplate.vercel.app',
-        name:'Adventure',
-        description:'Explore your adventures with our <b>React.js</b> adventure portfolio website template. Crafted for explorers, this template encapsulates the essence of adventurous spirits, offering a dynamic showcase of your journeys. With all essential elements included, from stunning visuals to interactive maps and captivating narratives, its your perfect digital companion for sharing and reliving your thrilling experiences. Ready to inspire and engage fellow adventurers, this template is designed to elevate your portfolio to new heights.'
+        id: '4',
+        image: adventure,
+        link: 'https://adventuretemplate.vercel.app',
+        name: 'Adventure',
+        description: 'Explore your adventures with our <b>React.js</b> adventure portfolio website template. Crafted for explorers, this template encapsulates the essence of adventurous spirits, offering a dynamic showcase of your journeys. With all essential elements included, from stunning visuals to interactive maps and captivating narratives, its your perfect digital companion for sharing and reliving your thrilling experiences. Ready to inspire and engage fellow adventurers, this template is designed to elevate your portfolio to new heights.'
     },
     {
-        id:'4',
+        id: '5',
         image: UrbanAbodes,
-        link:'https://urbanabodes-realestate.vercel.app',
-        name:'UrbanAbodes',
-        description:'UrbanAbodes is a dynamic real estate website developed using <b>React.js</b> , designed to simplify the process of discovering and purchasing properties. Users can effortlessly browse and search for new properties, view detailed listings with high-quality images, and filter results based on their preferences. The site offers a user-friendly interface, ensuring a seamless experience for both buyers and sellers, making it a comprehensive tool for anyone looking to invest in real estate.'
+        link: 'https://urbanabodes-realestate.vercel.app',
+        name: 'UrbanAbodes',
+        description: 'UrbanAbodes is a dynamic real estate website developed using <b>React.js</b> , designed to simplify the process of discovering and purchasing properties. Users can effortlessly browse and search for new properties, view detailed listings with high-quality images, and filter results based on their preferences. The site offers a user-friendly interface, ensuring a seamless experience for both buyers and sellers, making it a comprehensive tool for anyone looking to invest in real estate.'
     },
     {
-        id:'5',
+        id: '6',
         image: LuxeRooms,
-        link:'https://luxe-rooms.vercel.app',
-        name:'LuxeRooms',
-        description:'LuxeRooms is an elegant interior furniture website template built with <b>React.js</b> , offering a curated collection of premium furniture to transform your living spaces.'
+        link: 'https://luxe-rooms.vercel.app',
+        name: 'LuxeRooms',
+        description: 'LuxeRooms is an elegant interior furniture website template built with <b>React.js</b> , offering a curated collection of premium furniture to transform your living spaces.'
     },
     {
-        id:'6',
+        id: '7',
         image: Humanica,
-        link:'https://humanica.vercel.app',
-        name:'Humanica',
-        description:'Humanica an Employee Management Admin Panel I developed this website as a <b>Freelancer for my client from the Netherlands. Build using MERN stack</b>, Humanica is an employee management system that allows admins to control working hours, breaks, and vacation statuses. It features a fully functional calendar for monitoring specific dates and tracks the last 10 login IP addresses and locations, helping to identify which admins are logged in from specific locations. The system also supports registering multiple admins as needed.'
+        link: 'https://humanica.vercel.app',
+        name: 'Humanica',
+        description: 'Humanica an Employee Management Admin Panel I developed this website as a <b>Freelancer for my client from the Netherlands. Build using MERN stack</b>, Humanica is an employee management system that allows admins to control working hours, breaks, and vacation statuses. It features a fully functional calendar for monitoring specific dates and tracks the last 10 login IP addresses and locations, helping to identify which admins are logged in from specific locations. The system also supports registering multiple admins as needed.'
     },
+
 
 ]
 
 export const aboutdet = [
     {
-        name:'About me.',
-        description:'I\'m a MERN stack developer, distinguished by my graduation in BCA,crafting sophisticated web applications with MongoDB, Express.js, React.js, and Node.js. With a fervent passion for innovation,I transform challenges into sleek,user-centric experiences. Each project is a testament to my commitment to quality and cutting-edge development.'
+        name: 'About me.',
+        description: 'I\'m a MERN stack developer, distinguished by my graduation in BCA,crafting sophisticated web applications with MongoDB, Express.js, React.js, and Node.js. With a fervent passion for innovation,I transform challenges into sleek,user-centric experiences. Each project is a testament to my commitment to quality and cutting-edge development.'
     }
 ]
 
 export const servicedet = [
     {
-        id:'1',
-        image: web4 ,
-        name:'Web Development'
+        id: '1',
+        image: web4,
+        name: 'Web Development'
     },
     {
-        id:'2',
+        id: '2',
         image: web1,
-        name:'Web Design'
+        name: 'Web Design'
     },
     {
-        id:'3',
-        image:web2,
-        name:'UI/UX'
+        id: '3',
+        image: web2,
+        name: 'UI/UX'
     }
 ]
